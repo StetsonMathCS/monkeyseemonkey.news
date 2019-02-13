e607a52bcdf085176d7c5813edb0e81f213bf18c
Jordan Tyus <jordantyus@Jordans-MBP.ad.stetson.edu>
2019-02-13 12:41:03 -0500

    Created Home Page folder and displays logo through App.js

--- front-end/src/Components/Mission-Statement/mission-statement.js
+++ front-end/src/Components/Mission-Statement/mission-statement.js
@@ -1,16 +1,10 @@
 import React from 'react';
-
 const Mission = () => {
     return (
+        
         <div>
-            <br>
-            </br>
-            <center><img src="templogo.png" alt="Temp Logo"></img></center>
-            <form className ="example" action="/action_page.php" style="margin:auto;max-width:700px">
-            <input type="text" placeholder="Search.." name="search2">
-            <button type="submit"><i class="fa fa-search"></i></button>
-            </input>
-        </form>
+           <p><center>Hello World</center></p>
+            
         </div>
     );
 }

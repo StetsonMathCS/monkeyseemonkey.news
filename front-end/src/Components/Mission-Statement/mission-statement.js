396cde60216f054ce4706a6faa637190ea71b3dd
Jordan Tyus <jordantyus@Jordans-MBP.ad.stetson.edu>
2019-02-13 12:09:19 -0500

    Attempt at search in mission file

--- front-end/src/Components/Mission-Statement/mission-statement.js
+++ front-end/src/Components/Mission-Statement/mission-statement.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Mission = () => {
     return (
-        <p>Hello World!</p>
+        <div>
+            <br>
+            </br>
+            <center><img src="templogo.png" alt="Temp Logo"></img></center>
+            <form className ="example" action="/action_page.php" style="margin:auto;max-width:700px">
+            <input type="text" placeholder="Search.." name="search2">
+            <button type="submit"><i class="fa fa-search"></i></button>
+            </input>
+        </form>
+        </div>
     );
 }
 

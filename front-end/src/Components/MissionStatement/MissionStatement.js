b720229dc4a85098c2cf5c399a25187a37b6c077
Freddie Wittmann <fwittmann@stetson.edu>
2019-02-14 17:57:23 -0500

    Mission Statement Draft

--- front-end/src/Components/MissionStatement/MissionStatement.js
+++ front-end/src/Components/MissionStatement/MissionStatement.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Logo from '../Logo/Logo.png'
+console.log(Logo);
 const MissionStatement = () => {
     return (
         
-        <div>
-           <p><center>Hello World</center></p>
-            
+        <div class = "container mx-auto">
+            <center><img src={Logo} alt="Temp Logo"></img></center>
+           <h1><center>Our Mission Statement</center></h1>
+            <p>Here at Monkeyseemonkey.news, we believe that everybody should have consistent access to reliable
+                and trustworthy news sources. As part of our software model, we deploy Artificial Intelligence to crawl 
+                the web and filter out any bias that we so commonly see in our modern news sources. We have no prejudice,
+                and our goal is to provide our users with 100% accuracy in regards to their news searches.
+            </p>
         </div>
     );
 }

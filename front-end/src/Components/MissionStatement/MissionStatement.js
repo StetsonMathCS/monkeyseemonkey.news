08832ab49369aabcc1d2ed790da0dbc5145ce628
Freddie Wittmann <fwittmann@stetson.edu>
2019-02-14 18:09:52 -0500

    update

--- front-end/src/Components/MissionStatement/MissionStatement.js
+++ front-end/src/Components/MissionStatement/MissionStatement.js
@@ -5,13 +5,16 @@ const MissionStatement = () => {
     return (
         
         <div class = "container mx-auto">
-            <center><img src={Logo} alt="Temp Logo"></img></center>
+            <center><img src={Logo} alt="Temp Logo"></img></center> 
            <h1><center>Our Mission Statement</center></h1>
-            <p>Here at Monkeyseemonkey.news, we believe that everybody should have consistent access to reliable
-                and trustworthy news sources. As part of our software model, we deploy Artificial Intelligence to crawl 
-                the web and filter out any bias that we so commonly see in our modern news sources. We have no prejudice,
+           <br />
+           <center>
+            <p>Here at Monkeyseemonkey.news, we believe that everybody should have consistent access to reliable<br />
+                and trustworthy news sources. As part of our software model, we deploy Artificial Intelligence to crawl <br />
+                the web and filter out any bias that we so commonly see in our modern news sources. We have no prejudice, <br />
                 and our goal is to provide our users with 100% accuracy in regards to their news searches.
             </p>
+            </center>
         </div>
     );
 }

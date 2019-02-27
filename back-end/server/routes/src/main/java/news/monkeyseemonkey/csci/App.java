3f8ab45b8e5a19a53b34e52f748ead0cb368dc93
GregF <gfichthorn@stetson.edu>
2019-02-27 13:10:08 -0500

    doing stuff

--- back-end/server/routes/src/main/java/news/monkeyseemonkey/csci/App.java
+++ back-end/server/routes/src/main/java/news/monkeyseemonkey/csci/App.java
@@ -1,5 +1,6 @@
 package news.monkeyseemonkey.csci;
 import static spark.Spark.*;
+import com.google.gson.Gson;
 
 /**
  * Hello world!
@@ -9,6 +10,12 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Gson gson = new Gson();
+    	String app = gson.toJson(App.class);
+    	System.out.println(app);
         get("/hello", (req, res) -> "Hello World");
+        get("/home", (req, res) -> "homepage");
+        get("/home/search", (req, res) -> "searchbar");
+        get("/home/results", (req, res) -> "results");
     }
 }

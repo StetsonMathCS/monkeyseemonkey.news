98a2be2ffd12789c5c82016ed32ee84a1bd570ca
GregF <gfichthorn@stetson.edu>
2019-02-27 12:48:50 -0500

    trip and greg meeting office hours on tuesday changes

---
+++ back-end/server/routes/src/main/java/news/monkeyseemonkey/csci/App.java
@@ -0,0 +1,14 @@
+package news.monkeyseemonkey.csci;
+import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}

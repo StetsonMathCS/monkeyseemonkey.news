740662e33659f6985c3f564e8e40695d979de188
Freddie Wittmann <fwittmann@stetson.edu>
2019-02-28 14:41:57 -0500

    Built Article Example Page

--- front-end/src/Components/Article/Article.js
+++ front-end/src/Components/Article/Article.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import Logo from '../Logo/Logo';
+import Logo from '../Logo/Logo.js';
 import trump from '../Logo/trump.jpg';
+import abc from '../Logo/abc.png';
+
 
 const Article = () => {
     return (
@@ -59,9 +61,32 @@ const Article = () => {
             </ul>
             <br />
             <center>
-                
-            </center>
-        
+            <div className="max-w-md w-full lg:flex">
+                <div className="flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+                <img src = {trump} width = "400" height = "500"/>
+                </div>
+                <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                    <div className="mb-8">
+                    <p className="text-sm text-grey-dark flex items-center">
+                        
+                    </p>
+                    <div className="text-black font-bold text-xl mb-2">Our Closest Match!</div>
+                    <p className="text-grey-darker text-base">We found an article from ABC that passes our reliability metrics! If you are 
+                    looking for some reliable news, this article would be a good place to start!</p>
+                    </div>
+                    <div className="flex items-center w-full">
+                    <img className="w-10 h-10 rounded-full mr-4" src = {abc}/>
+                    
+                        <div className="text-sm">
+                            <p className="text-black leading-none">% Reliability</p>
+                            <p className="text-grey-dark">86%</p>
+                        </div>
+                </div>
+                </div>
+                </div>
+        </center>
+        <br />
+        <br />
         </div>
     );
 }

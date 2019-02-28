910ecfc005f3489a347c2f903d66df24e3df662a
Freddie Wittmann <fwittmann@stetson.edu>
2019-02-28 13:25:33 -0500

    working on example article compilation

--- front-end/src/Components/Article/Article.js
+++ front-end/src/Components/Article/Article.js
@@ -7,9 +7,21 @@ const Article = () => {
         <div className="container mx-auto bg-black">
         <center>
             <Logo/>
-            <h1 className = "text-white font-mono">Your Bias-free Article Compilation: </h1>
-            <img src = {trump} alt = "Non-biased image of Trump" />
-        </center>
+            <h1 className = "text-green-lighter font-bold">Donald Trump Declares Border Problems a National Emergency </h1>
+            <br />
+            <br />
+            <img src = {trump} alt = "Non-biased image of Trump" height = "600" width = "800" />
+            <br />
+            <br />
+            <br />
+            </center>
+            <ul className = "text-white font-mono font-bold pl-32 text-xl">
+                <li>
+                President Trump has declared a nationial emergency regarding the funding of the border-wall between Mexico and the 
+                United States
+                </li>
+            </ul>
+        
         </div>
     );
 }

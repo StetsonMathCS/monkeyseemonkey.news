5e362d4243eb61fc06f8e31de50e4cd87a357e56
hassan <halshamrani@stetson.edu>
2019-02-27 23:58:46 -0500

    Did the python access

---
+++ Database/access.py
@@ -0,0 +1,6 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='monkey', password='epJiphQuitmeoneykbet',
+                              host='localhost',
+                              database='monkey')
+cnx.close()

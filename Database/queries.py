67343e6a964936018a3174863b3aa719da76a276
hassan <halshamrani@stetson.edu>
2019-03-01 07:17:31 -0500

    Trying to do the queries.py

---
+++ Database/queries.py
@@ -0,0 +1,14 @@
+import datetime
+import mysql.connector
+
+cnx = mysql.connector.connect(user='monkey', database='epJiphQuitmeoneykbet')
+cursor = cnx.cursor()
+
+query = ("SELECT web_address, title, scoreFROM articles ")
+
+query = ("SELECT title, summary FROM summary ")
+
+query = ("SELECT id_article, id_summary FROM artsum ")
+
+cursor.close()
+cnx.close()

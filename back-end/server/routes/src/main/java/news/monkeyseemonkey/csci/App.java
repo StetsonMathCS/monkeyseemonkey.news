aebdd939405dd9e5b2cf7a3d0a35881d967cd330
GregF <gfichthorn@stetson.edu>
2019-03-01 12:49:47 -0500

    finished creating routes for our HTTP server that the frontend team can use via giving JSON data. have a great spring break - greg, charles, trip

--- back-end/server/routes/src/main/java/news/monkeyseemonkey/csci/App.java
+++ back-end/server/routes/src/main/java/news/monkeyseemonkey/csci/App.java
@@ -1,21 +1,29 @@
 package news.monkeyseemonkey.csci;
 import static spark.Spark.*;
+
+import org.eclipse.jetty.client.api.Request;
+
 import com.google.gson.Gson;
 
 /**
- * Hello world!
- *
+ * @author Greg Fichthorn Charles Mercenit Boyd Compton
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	Gson gson = new Gson();
-    	String app = gson.toJson(App.class);
-    	System.out.println(app);
-        get("/hello", (req, res) -> "Hello World");
-        get("/home", (req, res) -> "homepage");
-        get("/home/search", (req, res) -> "searchbar");
-        get("/home/results", (req, res) -> "results");
-    }
+	public static void main( String[] args )
+	{
+		Gson gson = new Gson();
+		
+		String home = "/home";
+		System.out.println(gson.toJson(home));
+		get(home, (req, res) -> "homepage");
+		
+		String home_search = "/home/search";
+		System.out.println(gson.toJson(home_search));
+		get(home_search, (req, res) -> "searchbar");
+		
+		String home_results = "/home/results";
+		System.out.println(gson.toJson(home_results));
+		get(home_results, (req, res) -> "results");
+	}
 }

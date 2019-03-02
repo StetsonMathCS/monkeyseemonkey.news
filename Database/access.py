73d0fceb3a71e855a92e8f8f504106286a34873c
hassan <halshamrani@stetson.edu>
2019-03-01 19:16:57 -0500

    Transfer the queries to the access file

--- Database/access.py
+++ Database/access.py
@@ -4,3 +4,22 @@ cnx = mysql.connector.connect(user='monkey', password='epJiphQuitmeoneykbet',
                               host='localhost',
                               database='monkey')
 cnx.close()
+
+
+
+import datetime
+import mysql.connector
+
+cnx = mysql.connector.connect(user='monkey', database='epJiphQuitmeoneykbet')
+cursor = cnx.cursor()
+
+query = ("SELECT web_address, title, scoreFROM articles ")
+
+query = ("SELECT title, summary FROM summary ")
+
+query = ("SELECT id_article, id_summary FROM artsum ")
+
+cursor.close()
+cnx.close()
+
+

91fe3dbb2a7c77f5a00f25ad32eed4c389cb8d23
Nicholas Gilmet <ngilmet@stetson.edu>
2019-03-01 23:48:08 -0500

    Fixed sentence similarity testing

--- ai/similarity.py
+++ ai/similarity.py
@@ -5,52 +5,18 @@ import re
 spacy.prefer_gpu()
 nlp = spacy.load('en_core_web_md')
 
-alphabets= "([A-Za-z])"
-prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
-suffixes = "(Inc|Ltd|Jr|Sr|Co)"
-starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
-acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
-websites = "[.](com|net|org|io|gov)"
-digits = "([0-9])"
-
-def split_into_sentences(text):
-    text = " " + text + "  "
-    text = text.replace("\n"," ")
-    text = re.sub(prefixes,"\\1<prd>",text)
-    text = re.sub(websites,"<prd>\\1",text)
-    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
-    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
-    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
-    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
-    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
-    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
-    text = re.sub(digits + "[.]" + digits,"\\1<prd>\\2",text)
-    if "”" in text: text = text.replace(".”","”.")
-    if "\"" in text: text = text.replace(".\"","\".")
-    if "!" in text: text = text.replace("!\"","\"!")
-    if "?" in text: text = text.replace("?\"","\"?")
-    text = text.replace(".",".<stop>")
-    text = text.replace("?","?<stop>")
-    text = text.replace("!","!<stop>")
-    text = text.replace("<prd>",".")
-    sentences = text.split("<stop>")
-    sentences = sentences[:-1]
-    sentences = [s.strip() for s in sentences]
-    return sentences
-
 theFile = open("similarityTesting.txt",encoding="utf8")
 
 bigString = theFile.read(9999999)
-
-arr = split_into_sentences(bigString)
-
+doc = nlp(bigString)
+arr = []
+for sent in doc.sents:
+	arr.append(sent)
 
 for a in range(0,len(arr)-1):
     for b in range(a+1,len(arr)):
-        docA = nlp(arr[a])
-        docB = nlp(arr[b])
+        docA = arr[a]
+        docB = arr[b]
         temp = docA.similarity(docB)
         if(len(docA) == len(docB)):
             temp *= 1

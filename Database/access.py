42b748482e15284dce6a708ab57f71c40b9f8ee5
hassan <halshamrani@stetson.edu>
2019-03-11 13:03:06 -0400

    did the access python and queries

--- Database/access.py
+++ Database/access.py
@@ -1,23 +1,35 @@
 import mysql.connector
+import datetime
 
 cnx = mysql.connector.connect(user='monkey', password='epJiphQuitmeoneykbet',
                               host='localhost',
                               database='monkey')
-cnx.close()
 
-
-
-import datetime
-import mysql.connector
-
-cnx = mysql.connector.connect(user='monkey', database='monkey')
 cursor = cnx.cursor()
 
 query = ("SELECT * FROM articles ")
+cursor.execute(query)
+myresult = cursor.fetchall()
+
+for x in myresult:
+  print(x)
+
 
 query = ("SELECT * FROM summary ")
+cursor.execute(query)
+myresult = cursor.fetchall()
+
+for x in myresult:
+  print(x)
+
 
 query = ("SELECT *  FROM artsum ")
+cursor.execute(query)
+myresult = cursor.fetchall()
+
+for x in myresult:
+  print(x)
+
 
 cursor.close()
 cnx.close()

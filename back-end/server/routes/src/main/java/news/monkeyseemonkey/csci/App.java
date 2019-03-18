9b68a2746eb9928c225610dd8f7a0ad03c29fbb1
GregF <gfichthorn@stetson.edu>
2019-03-18 12:29:36 -0400

    switched to node.js, spark was rough

--- back-end/server/routes/src/main/java/news/monkeyseemonkey/csci/App.java
+++ back-end/server/routes/src/main/java/news/monkeyseemonkey/csci/App.java
@@ -4,6 +4,9 @@ import static spark.Spark.*;
 import org.eclipse.jetty.client.api.Request;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 /**
  * @author Greg Fichthorn Charles Mercenit Boyd Compton
@@ -13,17 +16,23 @@ public class App
 	public static void main( String[] args )
 	{
 		Gson gson = new Gson();
+		JsonParser parser = new JsonParser();
 		
-		String home = "/home";
-		System.out.println(gson.toJson(home));
-		get(home, (req, res) -> "homepage");
+		// String id = what+I+am+searching
+		// split plusses what I am searching
+		// use search api to find article names
+		// respond with list of article names
 		
-		String home_search = "/home/search";
-		System.out.println(gson.toJson(home_search));
-		get(home_search, (req, res) -> "searchbar");
+		post("/search/:id", (req, res) -> {
+			JsonElement search = parser.parse(res);
+		});
 		
-		String home_results = "/home/results";
-		System.out.println(gson.toJson(home_results));
-		get(home_results, (req, res) -> "results");
+		
+		// string id = the+article+name
+		// split by the plusses
+		// respond (res) with the article name, date, story (array of bullet points), 
+		//array of sources, array of ratings
+		
+		get("/article/:id", (req, res) -> "results");
 	}
 }

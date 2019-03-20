e1669f84818c992404a08b56b1065d4644b5ae69
GregF <gfichthorn@stetson.edu>
2019-03-19 20:00:15 -0400

    working to get front end and back end projects up and running

--- back-end/server/NodeServer/nodeServer/proxy.js
+++
@@ -1,5 +0,0 @@
-var https = require('https');
-var httpProxy = require('http-proxy');
-var express = require('express');
-var HttpProxyRules = require('http-proxy-rules');
-
--- back-end/server/NodeServer/nodeServer/server.js
+++ back-end/server/NodeServer/nodeServer/server.js
@@ -1,35 +1,62 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
+var http = require('http');
+var fs = require('fs');
+
 app.use(express.json());
 app.use(cors());
-/*const knex = require('knex')({
 
+/*const knex = require('knex')({
 })
 */
+
+//here is a fake article information for 
+//JSON purposes for until we connect to database
+let database = [
+    article = [
+        {
+            name: "article name",
+            date: "article date",
+            id: "article id",
+            story: {
+                bullet1: "article bullet 1",
+                bullet2: "article bullet 2",
+                bullet3: "article bullet 3"
+            }
+        }
+    ]
+]
 
 app.get('/', (req, res) => {
     res.json('it is working!');
 })
 
 app.post('/searchresults/:id', (req, res) => {
-    const query = req.body.query;
-
-    
-    //respond with a list of article names
+    for (let article of database) {
+        if (req.body.name === article.name) {
+            res.json(article);
+        }
+    }
+    //respond all article names and attached dates
 })
 
 app.get('/article/:id', (req, res) => {
     const name = req.body.name;
-   /* knex('articles')
-        .select('*')
-        .from('articles')
-        .where({name: name})
-        .then(res.json(article))
-        */
+    //return article with matching name
 })
 
+/* old code
 app.listen(4567, () => {
     console.log("app is running on port 4567");
+})*/
+
+fs.readFile('./index.html', function(err, html){
+    if(err) throw err;
+
+    http.createServer(function(req, res) {
+        res.writeHeader(200, {"Content-Type": "text/html"});
+        res.write(html);
+        res.end();
+    }).listen(4567);
 })

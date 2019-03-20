8b056aef36c32f5483a15391a6d01445b73a3619
Freddie Wittmann <fwittmann@stetson.edu>
2019-03-20 12:20:23 -0400

    update

--- front-end/src/Components/ListItem/ListItem.js
+++ front-end/src/Components/ListItem/ListItem.js
@@ -9,15 +9,15 @@ const ListItem = () => {
         <Link to={`/article/:genericarticle`}>
             <div className="max-w-md w-full lg:flex rounded overflow-hidden shadow-lg ">
                 <div className="flex items-stretch bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center ">
-                <img src = {trump_wall} width = "800" height = "500" className = "rounded"/>
+                <img src = {trump_wall} width = "500" height = "500" className = "rounded"/>
                 </div>
                 <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal container mx-auto bg-blue-darkest">
                     <div className="mb-8 container mx-auto bg-blue-darkest">
                     <div className="font-bold text-xl mb-2 text-center text-white " >Donald Trump Declares Border Problems a National Emergency </div>
-                    <p className="text-grey-darker text-base">Here is a compiled and <b>unbiased</b> news report regarding your topic!</p>
+                    <p className="text-grey-darker text-base">President Trump has declared a nationial emergency regarding the funding of the border-wall between Mexico and the United States...</p>
                     </div>
                     <div className="flex items-center w-full container mx-auto bg-blue-darkest">
-                    <img className="w-10 h-10 rounded-full mr-4" src = {abc}/>
+                    <h1><b>SOURCES</b></h1>
                     
                         <div className="text-sm container mx-auto bg-blue-darkest">
                             <p className="text-black leading-none">% Reliability</p>

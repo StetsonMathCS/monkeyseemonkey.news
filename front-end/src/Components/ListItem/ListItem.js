7b4c3f37267006e0a219744dd6429daab1184b70
Freddie Wittmann <fwittmann@stetson.edu>
2019-03-20 19:52:20 -0400

    changed colors to look nicer/more modern

--- front-end/src/Components/ListItem/ListItem.js
+++ front-end/src/Components/ListItem/ListItem.js
@@ -8,18 +8,18 @@ import trump_wall_card from '../Logo/trump_wall_card.jpg';
 const ListItem = () => {
     return (
         <Link to={`/article/:genericarticle`}>
-            <div className="max-w-md w-full lg:flex rounded overflow-hidden shadow-lg ">
+            <div className="max-w-md w-full lg:flex rounded shadow-lg ">
                 <div className="flex items-stretch bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center ">
                 <img src = {trump_wall} width = "700" height = "500" className = "rounded"/>
                 </div>
-                <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal container mx-auto bg-blue-darkest">
-                    <div className="mb-8 container mx-auto bg-blue-darkest">
-                    <div className="font-bold text-xl mb-2 text-center text-white " >Donald Trump Declares Border Problems a National Emergency </div>
-                    <p className="text-grey-darker text-base">President Trump has declared a nationial emergency regarding the funding of the border-wall between Mexico and the United States...</p>
+                <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal container mx-auto bg-blue-darker">
+                    <div className="mb-8 container mx-auto bg-blue-darker">
+                    <div className="font-bold text-xl mb-2 text-center text-green-lighter" >Donald Trump Declares Border Problems a National Emergency </div>
+                    <p className="text-white text-base">President Trump has declared a nationial emergency regarding the funding of the border-wall between Mexico and the United States...</p>
                     </div>
-                    <div className="flex items-center w-full container mx-auto bg-blue-darkest">
+                    <div className="flex items-center w-full container mx-auto bg-blue-darker">
                     <h3 className = "pt-3 pb-5 text-green-lighter font-bold">SOURCES:</h3>
-                        <div className="text-sm container mx-auto bg-blue-darkest">
+                        <div className="text-sm container mx-auto bg-blue-darker">
                             <p className="mb-2 text-white">abc, Fox News, CNN</p>
                         </div>
                     </div>
@@ -30,14 +30,14 @@ const ListItem = () => {
             <div className="flex items-stretch bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center ">
             <img src = {trump_wall_card} width = "600" height = "500" className = "rounded"/>
             </div>
-                           <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal container mx-auto bg-blue-darkest">
-                    <div className="mb-8 container mx-auto bg-blue-darkest">
-                    <div className="font-bold text-xl mb-2 text-center text-white " >Trump's border wall could impact upstate military sites</div>
-                    <p className="text-grey-darker text-base">An Air Force research lab at the mothballed Griffiss Air Force Base in Oneida County is facing a $14.2 million loss...</p>
+                           <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal container mx-auto bg-blue-darker">
+                    <div className="mb-8 container mx-auto bg-blue-darker">
+                    <div className="font-bold text-xl mb-2 text-center text-green-lighter" >Trump's border wall could impact upstate military sites</div>
+                    <p className="text-white text-base">An Air Force research lab at the mothballed Griffiss Air Force Base in Oneida County is facing a $14.2 million loss...</p>
                     </div>
-                    <div className="flex items-center w-full container mx-auto bg-blue-darkest">
+                    <div className="flex items-center w-full container mx-auto bg-blue-darker">
                     <h3 className = "pt-3 pb-5 text-green-lighter font-bold">SOURCES:</h3>
-                        <div className="text-sm container mx-auto bg-blue-darkest">
+                        <div className="text-sm container mx-auto bg-blue-darker">
                             <p className="mb-2 text-white">PBS, timesunion, CNN, FOX</p>
                         </div>
                 </div>

8c9de19c4920d4c666b35434f78225775a619749
DESKTOP-6E2KC2A\Samue <sbaird@stetson.edu>
2019-03-24 14:56:49 -0400

    added json data integration to listitem

--- front-end/src/Components/ListItem/ListItem2.js
+++ front-end/src/Components/ListItem/ListItem2.js
@@ -5,18 +5,18 @@ import abc from '../Logo/abc.png';
 import trump_wall from '../Logo/trump_wall.jpg';
 import trump_wall_card from '../Logo/trump_wall_card.jpg';
 
-const ListItem2 = () => {
+const ListItem2 = (key, title, description, image) => {
     return (
-        <Link to={`/article/:genericarticle`}>
+        <Link to={`/article/${String(title).split("+").join(" ")}`}>
             <div className="max-w-md lg:flex">
             <div className>
-            <img src = {trump_wall_card} max-height = "auto" className = "rounded-l"/>
+            <img src = {image} max-height = "auto" className = "rounded-l"/>
             </div>
-                           <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal bg-blue-darker">
+                <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal bg-blue-darker">
                     <div className="mb-8 container mx-auto bg-blue-darker">
-                    <div className="font-bold text-xl mb-2 text-center text-green-lighter" >Trump's border wall could impact upstate military sites</div>
+                    <div className="font-bold text-xl mb-2 text-center text-green-lighter" >{String(title)}</div>
                     <div height = "auto">                    
-                    <p className="text-white text-base">An Air Force research lab at the mothballed Griffiss Air Force Base in Oneida County is facing a $14.2...</p>
+                    <p className="text-white text-base">{String(description)}</p>
                     </div>
                     </div>
                 </div>

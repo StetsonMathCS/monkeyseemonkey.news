7f3b74cb64b3d0679162ef3fbf390d517e64536a
Freddie Wittmann <fwittmann@stetson.edu>
2019-03-24 15:04:16 -0400

    minor change

--- front-end/src/Components/ListItem/ListItem2.js
+++ front-end/src/Components/ListItem/ListItem2.js
@@ -10,19 +10,11 @@ const ListItem2 = (key, title, description, image) => {
         <Link to={`/article/${String(title).split("+").join(" ")}`}>
             <div className="max-w-md lg:flex">
             <div className>
-<<<<<<< HEAD
             <img src = {trump_wall_card} max-height = "auto" className = "rounded-l lg:border-t-1"/>
-=======
-            <img src = {image} max-height = "auto" className = "rounded-l"/>
->>>>>>> 8c9de19c4920d4c666b35434f78225775a619749
             </div>
                 <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal bg-blue-darker">
                     <div className="mb-8 container mx-auto bg-blue-darker">
-<<<<<<< HEAD
                     <div className="font-bold text-xl mb-2 text-center text-green-lighter">Trump's border wall could impact upstate military sites</div>
-=======
-                    <div className="font-bold text-xl mb-2 text-center text-green-lighter" >{String(title)}</div>
->>>>>>> 8c9de19c4920d4c666b35434f78225775a619749
                     <div height = "auto">                    
                     <p className="text-white text-base">{String(description)}</p>
                     </div>

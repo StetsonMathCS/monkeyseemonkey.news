90ff55e5ab4a731a5ec835a65afdc8f64a69a9c8
Freddie Wittmann <fwittmann@stetson.edu>
2019-03-24 14:47:51 -0400

    still need images lined up on bottom of card

--- front-end/src/Components/ListItem/ListItem.js
+++ front-end/src/Components/ListItem/ListItem.js
@@ -8,39 +8,31 @@ import trump_wall_card from '../Logo/trump_wall_card.jpg';
 const ListItem = () => {
     return (
         <Link to={`/article/:genericarticle`}>
-            <div className="max-w-md lg:flex rounded shadow-lg">
-                <div className="items-stretch bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center ">
-                <img src = {trump_wall} width = "700" height = "400" className = "rounded"/>
+            <div className="max-w-md lg:flex" width = "800">
+                <div className>
+                <img src = {trump_wall} max-height = "auto" className = "rounded-l border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light"/>
                 </div>
                 <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal container mx-auto bg-blue-darker">
                     <div className="mb-8 container mx-auto bg-blue-darker">
                     <div className="font-bold text-xl mb-2 text-center text-green-lighter" >Donald Trump Declares Border Problems a National Emergency </div>
-                    <p className="text-white text-base">President Trump has declared a nationial emergency regarding the funding of the border-wall between Mexico and the United States...</p>
+                    <div>
+                    <p className="text-white text-base">President Trump adsfhas declared a nationial emergency regarding the funding of the border-wall between...</p>
                     </div>
-                    <div className="flex items-center w-full container mx-auto bg-blue-darker">
-                    <h3 className = "pt-3 pb-5 text-green-lighter font-bold">SOURCES:</h3>
-                        <div className="text-sm container mx-auto bg-blue-darker">
-                            <p className="mb-2 text-white">abc, Fox News, CNN</p>
-                        </div>
                     </div>
                     </div>
                     </div>
                 <br />
-            <div className="max-w-md w-full lg:flex rounded overflow-hidden shadow-lg ">
-            <div className="items-stretch bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center ">
-            <img src = {trump_wall_card} width = "600" height = "800" className = "rounded" height = "auto"/>
+            <div className="max-w-md lg:flex">
+            <div className>
+            <img src = {trump_wall_card} max-height = "auto" className = "rounded-l"/>
             </div>
-                           <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal container mx-auto bg-blue-darker">
+                           <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal bg-blue-darker">
                     <div className="mb-8 container mx-auto bg-blue-darker">
                     <div className="font-bold text-xl mb-2 text-center text-green-lighter" >Trump's border wall could impact upstate military sites</div>
-                    <p className="text-white text-base">An Air Force research lab at the mothballed Griffiss Air Force Base in Oneida County is facing a $14.2 million loss...</p>
+                    <div height = "auto">                    
+                    <p className="text-white text-base">An Air Force research lab at the mothballed Griffiss Air Force Base in Oneida County is facing a $14.2...</p>
                     </div>
-                    <div className="flex items-center w-full container mx-auto bg-blue-darker">
-                    <h3 className = "pt-3 pb-5 text-green-lighter font-bold">SOURCES:</h3>
-                        <div className="text-sm container mx-auto bg-blue-darker">
-                            <p className="mb-2 text-white">PBS, timesunion, CNN, FOX</p>
-                        </div>
-                </div>
+                    </div>
                 </div>
                 </div>
                 <br />

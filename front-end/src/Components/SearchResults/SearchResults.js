8675252eb374f383e1c15569f6331739278a0d82
DESKTOP-6E2KC2A\Samue <sbaird@stetson.edu>
2019-03-25 12:54:31 -0400

    made a list item alternative

--- front-end/src/Components/SearchResults/SearchResults.js
+++ front-end/src/Components/SearchResults/SearchResults.js
@@ -3,7 +3,7 @@ import ListItem from '../ListItem/ListItem';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Logo from '../Logo/Logo.js';
 import Search from '../Search/Search.js'
-
+import GridItem from '../ListItem/GridItem'
 class SearchResults extends Component {
     constructor(props) {
         super(props);
@@ -34,15 +34,15 @@ class SearchResults extends Component {
     render() {
         return (
             <div className = "container mx-auto bg-blue-darkest" >
-            <center>
-            <Logo />
-            <Search />
-            <br />
-            <h1 className = "pt-3 pb-5 text-green-lighter font-bold">Your Search Results!</h1>
-            <div>
-                <ListItem/>
-            </div>
-            </center>
+                <center>
+                <Logo />
+                <Search />
+                <br />
+                <h1 className = "pt-3 pb-5 text-green-lighter font-bold">Your Search Results!</h1>
+                <div>
+                    <GridItem/>
+                </div>
+                </center>
             </div>
         );
     }

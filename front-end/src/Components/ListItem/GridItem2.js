e6858c394de5322527ad6565d07e3362bca02b58
Freddie Wittmann <fwittmann@stetson.edu>
2019-03-26 18:44:18 -0400

    somebody try this on FireFox

--- front-end/src/Components/ListItem/GridItem2.js
+++ front-end/src/Components/ListItem/GridItem2.js
@@ -8,11 +8,11 @@ import './GridItem.css'
 
 const GridItem2 = () => {
     return (
-        <Link to={`/article/:genericarticle`} className="grid hover:border-white bg-blue-darker border-r-2 border-b-2 border-l-2 border-t-2 border-blue rounded">
-            <img src = {trump_wall} className = "gridimg rounded-l"/>
-            <div className="gridtext grid2">
-                <h1 className="font-bold mb-2 text-center text-green-lighter pt-5">asdfasdf</h1>
-                <div className="text-grey text-base"> goodbyeasdf </div>
+        <Link to={`/article/:genericarticle`} className="grid hover:border-white bg-blue-darker border-r-2 border-b-2 border-l-2 border-t-2 border-blue-darker hover:border-blue rounded">
+            <img src = {trump_wall_card} className = "gridimg rounded-l"/>
+            <div id = "display" className="gridtext grid2">
+                <h2 id = "positioner" className="font-bold mb-2 text-center text-green-lighter pt-5">Trump's border wall could impact <br />upstate military sites</h2>
+                <div className="text-grey text-base pt-5">WASHINGTON — A handful of military sites in upstate New York could be impacted if President Donald J. Trump’s plan to divert Pentagon construction funds...</div>
             </div>
             <br />
             <br />

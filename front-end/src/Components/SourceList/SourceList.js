21cce45c4641a6ce94f491f05f895188d235d4fd
GregF <gfichthorn@stetson.edu>
2019-03-27 12:36:08 -0400

    SourceList table with article name, publisher, date

--- front-end/src/Components/SourceList/SourceList.js
+++ front-end/src/Components/SourceList/SourceList.js
@@ -6,36 +6,39 @@ class SourceList extends Component {
   constructor(props) {
     super(props);
     }
-  
 
-
-  // title: sources                                         //
-  // make a table with columns and rows and stuff           //
-  // publisher, title, date published (and column labels)   //
+  // title: sources                                         
+  // make a table with columns and rows and stuff           
+  // publisher, title, date published (and column labels)   
   // the title will be a link to the article                
   // publisher: link to publisher as well as logo           
-  // centered, same size font as article, white font        //
+  // centered, same size font as article, white font        
   // background color change to light grey/blue             
-  // make sure is seperated for beautiful                   
+  // make sure is seperated for beautiful     
   render() {
     return (
-      <div>
+      <div className="bg-blue-darker">
         <h1 className = "center text-white font-bold text-xl mb-2"> Sources </h1>
-        <table className = "w-full"> 
+        <table className = "w-full text-white font-mono font-bold pb-5 text-xl"> 
           <tr>
-            <th className = "text-white font-mono font-bold pb-5 text-xl"> Title + link </th>
-            <th className = "text-white font-mono font-bold pb-5 text-xl"> Publisher + link </th>
-            <th className = "text-white font-mono font-bold pb-5 text-xl"> Date Published </th>
+            <th> Article Name </th>
+            <th> Publisher </th>
+            <th> Date Published </th>
           </tr>
           <tr>
-            <th className = "text-white font-mono font-bold pb-5 text-xl"> Article 1 </th>
-            <th className = "text-white font-mono font-bold pb-5 text-xl"> Publisher 1 </th>
-            <th className = "text-white font-mono font-bold pb-5 text-xl"> Date 1 </th>
+            <th> <a href="https://www.google.com/">Article 1</a> </th>
+            <th> <a href="https://www.google.com/">Publisher 1</a> </th>
+            <th> Date 1 </th>
           </tr>
           <tr>
-            <th className = "text-white font-mono font-bold pb-5 text-xl"> Article 2 </th>
-            <th className = "text-white font-mono font-bold pb-5 text-xl"> Publisher 2 </th>
-            <th className = "text-white font-mono font-bold pb-5 text-xl"> Date 2 </th>
+            <th> <a href="https://www.google.com/">Article 2</a> </th>
+            <th> <a href="https://www.google.com/">Publisher 2</a> </th>
+            <th> Date 2 </th>
+          </tr>
+          <tr>
+            <th> <a href="https://www.google.com/">Article 3</a> </th>
+            <th> <a href="https://www.google.com/">Publisher 3</a> </th>
+            <th> Date 3 </th>
           </tr>
         </table>
       </div>

03d342d7179c380b70c97ef887b73204297c0b0f
Freddie Wittmann <fwittmann@stetson.edu>
2019-03-27 13:24:10 -0400

    removed images from search list

--- front-end/src/Components/ListItem/GridItem3.js
+++ front-end/src/Components/ListItem/GridItem3.js
@@ -9,7 +9,6 @@ import trump_wall_card from '../Logo/trump_wall_card.jpg';
 const GridItem3 = () => {
     return (
         <Link to={`/article/:genericarticle`} className="grid hover:border-blue bg-blue-darker border-r-2 border-b-2 border-l-2 border-t-2 border-blue-darker rounded">
-            <img src = {abc} className = "gridimg rounded-l"/>
             <div id = "display" className="gridtext grid2">
                 <h2 id = "positioner" className="font-bold mb-2 text-center text-green-lighter pt-5">We have generated an unbiased article  <br /> that matches your search!</h2>
                 <div className="text-grey text-base pt-5">Acting Defense Secretary Patrick Shanahan approved a plan on Monday for the Pentagon to transfer $1 billion towards the construction of 57 miles...</div>

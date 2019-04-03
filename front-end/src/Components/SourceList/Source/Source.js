a60e73dd78a7d48204d23a5324e2e898e5204311
GregF <gfichthorn@stetson.edu>
2019-04-03 12:47:01 -0400

    in class commit avoid git fiasco - greg

---
+++ front-end/src/Components/SourceList/Source/Source.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../SourceList.css';
+import { totalmem } from 'os';
+
+
+const Source = ({title, publisher, date, titleLink}) => {
+    return (
+        <tr>
+            <th> <a href={titleLink}>{title}</a> </th>
+            <th> {publisher} </th>
+            <th> {date} </th>
+        </tr>
+    );
+}
+
+export default Source

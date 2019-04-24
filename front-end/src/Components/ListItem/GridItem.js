0ec45d70963137bd103c789d476fe3fb87585fff
DESKTOP-6E2KC2A\Samue <sbaird@stetson.edu>
2019-04-24 14:36:00 -0400

    fixed grid item styling

--- front-end/src/Components/ListItem/GridItem.js
+++ front-end/src/Components/ListItem/GridItem.js
@@ -6,10 +6,8 @@ const GridItem = ({title, summary}) => {
     return (
         <Link to={`/article/+${title.split(" ").join("+")}`}>
             <div className="grid hover:border-blue bg-blue-darker border-r-2 border-b-2 border-l-2 border-t-2 border-blue-darker rounded">
-                <div id = "display" className="gridtext grid2">
-                    <h2 id = "positioner" className="font-bold mb-2 text-center text-green-lighter pt-5">{`${title + ''}`}</h2>
+                    <h2 className="font-bold mb-2 text-center text-green-lighter pt-5">{`${title + ''}`}</h2>
                     <div className="text-grey text-base pt-5 px-5">{`${summary + ''}`}</div>
-                </div>
             </div>
             <br />
             <br />

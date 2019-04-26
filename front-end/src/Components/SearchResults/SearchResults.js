7fe0cf507893bf1ecc5ac28de8debac1a66efd02
DESKTOP-6E2KC2A\Samue <sbaird@stetson.edu>
2019-04-26 13:02:40 -0400

    infinite load testing

--- front-end/src/Components/SearchResults/SearchResults.js
+++ front-end/src/Components/SearchResults/SearchResults.js
@@ -3,18 +3,25 @@ import Logo from '../Logo/Logo.js';
 import Search from '../Search/Search.js'
 import { withRouter } from "react-router-dom";
 import GridItem from '../ListItem/GridItem'
+import InfiniteScroll from 'react-infinite-scroller'
 class SearchResults extends Component {
     constructor(props) {
         super(props);
         this.state = {
             search: String(props.match.params.id).split("+").join(" ").replace(/ /, ""),
             hasMore: true,
+            initialLoad: true,
             articles: [],
             start: 0
         };
     }
+    /*
+    shouldComponentUpdate(nextProps) {
+        return nextProps.location.pathname != this.props.location.pathname;
+    }
+    */
 
-    componentDidUpdate(prevProps) {
+    componentDidUpdate(prevProps, prevState) {
         if (this.props.location.pathname !== prevProps.location.pathname) {
             this.setState ({
             hasMore: true,
@@ -22,9 +29,13 @@ class SearchResults extends Component {
             start: 0
             });
             this.state.search = this.props.match.params.id;
-            this.loadItems();
         }
-        
+
+        if(prevState.initialLoad) {
+            this.setState({
+            initialLoad: false
+        });
+        }
     }
 
     componentDidMount() {
@@ -34,7 +45,6 @@ class SearchResults extends Component {
     loadItems() {
         let body = process.env.REACT_APP_URL + "/solr/monkey/selectq=summary%3A" + encodeURIComponent(this.state.search) + "&start=" + 0 + "&wt=json";
         let authorization = "Basic " + window.btoa(process.env.REACT_APP_USERNAME + ":" + process.env.REACT_APP_PASSWORD);
-        console.log(body);
         fetch(body, { 
             mode: "cors",
             headers: { "Content-Type": "application/json",
@@ -54,7 +64,8 @@ class SearchResults extends Component {
         .then(data => {
             if (Array.isArray(data.response.docs) || data.response.docs.length) {
                 data = data.response;
-                if(data.numFound <= 10) this.setState({hasMore: false});
+                data.docs = this.state.articles.concat(data.docs);
+                if(data.numFound <= this.state.start + 10) this.setState({hasMore: false});
                 this.setState({
                     articles: data.docs,
                     start: (this.state.start + 10)
@@ -70,11 +81,16 @@ class SearchResults extends Component {
     render() {
         let items = [];
         // eslint-disable-next-line
-        this.state.articles.map((article, i) => {
-            items.push(
-                <GridItem title={article.title[0]} summary={article.summary[0].split(".")[0].replace(/\+/, ".")} key={i}/>
-            );
-        });
+        if(this.state.articles.length) {
+            this.state.articles.map((article, i) => {
+                items.push(
+                    <GridItem title={article.title[0]} summary={article.summary[0].split(".")[0].replace(/\+/, ".")} key={i}/>
+                );
+            });
+        } else {
+            items.push(<h1 className="pt-0 text-green-lighter"> No Articles Found </h1>);
+            console.log("hi");
+        }
 
         return (
             <div className = "container mx-auto bg-blue-darkest" >
@@ -85,15 +101,16 @@ class SearchResults extends Component {
                 <br />
                 <br />
                 <div>
-                {/*}
+                {/*
                 <InfiniteScroll
                     pageStart={0}
                     loadMore={this.loadItems()}
-                    hasMore={false}
-                    initialLoad={true}
+                    hasMore={this.state.hasMore}
+                    initialLoad={this.state.initialLoad}
                     loader={<div>Loading ...</div>}
+                    useWindow={true}
                     >
-                    <div>{this.state.items}</div>
+                    {<div className="h-screen">{items}</div>}
                 </InfiniteScroll>
                 */}
                 {items}

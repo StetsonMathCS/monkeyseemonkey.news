554750125ba0e3f901684f49f4255afb03c17975
DESKTOP-6E2KC2A\Samue <sbaird@stetson.edu>
2019-04-30 20:46:50 -0400

    Fixed search bar unresponsive at smallest screen

--- front-end/src/Components/Logo/Logo.js
+++ front-end/src/Components/Logo/Logo.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
     return (
          <Link to={`/`} className="pb-0">
-            <img src={logo} alt="Logo" className="pb-0"/>
+            <img id="logo" src={logo} alt="Logo" className="pb-0"/>
         </Link>
     );
 }
--- front-end/src/Components/Search/Search.js
+++ front-end/src/Components/Search/Search.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter} from "react-router-dom";
+import "./Search.css";
 class Search extends Component {
     constructor(props) {
         super(props);
@@ -22,15 +23,13 @@ class Search extends Component {
     render() {
         return (
             <div className = "bg-blue-darkest" >
-                <center className = "center">
-                <div className="w-full max-w-sm">
-                        <form className="flex w-full items-center py-2 rounded-full bg-blue-darker" onSubmit = {() => this.searchSubmit()}>
-                            <input className="appearance-none bg-transparent border-none w-full text-white text-2xl ml-3 py-1 px-2 leading-tight focus:outline-none" type="text" onChange={(event) => this.onSearchChange(event)} value={this.state.search} aria-label="Full name" id ="test"  />
-                            <button type="submit" className="flex-no-shrink bg-blue hover:bg-blue-dark border-blue hover:border-blue-dark text-sm border-4 text-white py-4 px-8 mr-2 rounded-full" onClick = {() => this.searchSubmit()}>
+                <div className="">
+                        <form id="searchForm" className="flex flex sm:max-w-sm md:max-w-md py-2 rounded-full bg-blue-darker" onSubmit = {() => this.searchSubmit()}>
+                            <input size="1" className="flex-grow appearance-none bg-transparent border-none text-white text-2xl pl-6 py-1 leading-tight focus:outline-none" type="text" onChange={(event) => this.onSearchChange(event)} value={this.state.search} aria-label="Full name" id ="test"  />
+                            <button type="submit" id="searchSubmit" className="mr-2 relative bg-blue hover:bg-blue-dark border-blue hover:border-blue-dark border-4 text-white py-4 px-8 rounded-full xs:px-0" onClick = {() => this.searchSubmit()}>
                             </button>
                         </form>
                     </div>
-                </center>
             </div>
         );   
     } 
